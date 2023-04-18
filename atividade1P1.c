9957384dcd33d129fc816a8e82e900a0739704a4
Azzunzas <131110590+Azzunzas@users.noreply.github.com>
2023-04-17 23:48:19 -0300

    Update atividade1P1.c

--- atividade1P1.c
+++ atividade1P1.c
@@ -1,8 +1,9 @@
-//Pergunta 1
-//Escreva um programa para armazenar dinamicamente um vetor de inteiros contendo 1500 elementos. 
-//Atribua para cada elemento do vetor o valor do seu índice multiplicado por 2. Após isso, 
-//exiba na tela os 10 primeiros e os 10 últimos elementos do vetor. Por fim, libere a memória alocada para o vetor.
-
+/*
+Pergunta 1
+Escreva um programa para armazenar dinamicamente um vetor de inteiros contendo 1500 elementos. 
+Atribua para cada elemento do vetor o valor do seu índice multiplicado por 2. Após isso, 
+exiba na tela os 10 primeiros e os 10 últimos elementos do vetor. Por fim, libere a memória alocada para o vetor.
+*/
 #include <stdio.h>
 #include <stdlib.h>
  
