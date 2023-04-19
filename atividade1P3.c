2735952ab04f565368cbd8dd1a4a5cc7a16d6cff
Guilherme Assunção <131110590+Azzunzas@users.noreply.github.com>
2023-04-18 23:42:16 -0300

    Update atividade1P3.c

--- atividade1P3.c
+++ atividade1P3.c
@@ -1,4 +1,5 @@
-/*Escreva um programa que aloque dinamicamente uma estrutura para armazenar dados sobre veículos (fabricante, modelo, ano e preço), 
+/*
+Escreva um programa que aloque dinamicamente uma estrutura para armazenar dados sobre veículos (fabricante, modelo, ano e preço), 
 sendo que o número de registros (pelo menos 2 registros) é lido do teclado e a memória alocada deve ser definida em função do tamanho da estrutura.
  Após isso, exiba na tela apenas os dados do segundo veículo cadastrado. Por fim, libere a memória alocada para a estrutura.
 */
